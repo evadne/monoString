300374e280ad2db636dd2c0abbbc3e25e7498ff6
Evadne Wu <ev@monoceroi.com>
2010-07-12 10:07:29 +0800

    makes byteLength a getter method

--- lib.mono.string.js
+++ lib.mono.string.js
@@ -6,21 +6,36 @@
 
 
 
-String.prototype.byteLength = function () {
 
-//	Assume UTF-8
 
-	var totalBytes = 0;	
 
-	for (var i = 0; i < this.length; i ++) {
 
-		totalBytes += Math.ceil(Math.log(this.charCodeAt(i)) / Math.log(Math.pow(2, 8)));
+
+//	!Introspection
+
+	String.prototype.__defineGetter__("getByteLength", function () {
 	
+	//	Assume UTF-8
+	
+		var totalBytes = 0;	
+	
+		for (var i = 0; i < this.length; i ++) {
+	
+			totalBytes += Math.ceil(Math.log(this.charCodeAt(i)) / Math.log(Math.pow(2, 8)));
+		
+		}
+		
+		return totalBytes;
+	
+	});
+
+
+
+
+
 	}
 	
-	return totalBytes;
 
-}
 
 
 
